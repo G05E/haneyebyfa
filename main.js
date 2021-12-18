60a97666bbbd659cd4fdf5aa2e4e87294b54c149
G05E <96241112+G05E@users.noreply.github.com>
2021-12-18 13:17:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var a=prompt ("Welcome to Happy New Year website! If you want to see our Happy New Year Slides, Type Yes. If no, type No, Type No.");
+
+    if (a=="Yes"){
+      window.location.href="https://bit.ly/haneyebyfa"
+    }
+
+    if (a=="No"){
+      window.location.href="content.html"
+    }
